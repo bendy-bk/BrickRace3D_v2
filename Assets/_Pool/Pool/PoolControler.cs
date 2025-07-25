9d11e13b7989e1577b926d046572b9d09fc1f356
HiepTran <hiep61027@gmail.com>
2025-07-25 13:17:29 +0700

    FirstCm

---
+++ Assets/_Pool/Pool/PoolControler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PoolControler : MonoBehaviour
+{
+    [Header("Pooling")]
+    public PoolAmount[] Pool;
+
+    public void Awake()
+    {
+        for (int i = 0; i < Pool.Length; i++)
+        {
+            SimplePool.Preload(Pool[i].prefab, Pool[i].amount, Pool[i].root, Pool[i].collect, Pool[i].clamp);
+        }
+
+    }
+
+}
+
+
